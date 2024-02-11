953db2b7241c14fe3d00d7ff3699580d0e16cd61
rubini <rubinisakthivel07@gmail.com>
2024-02-11 21:39:22 +0530

    added extent report

---
+++ src/main/java/selenium/Selenium_S2_ExtentReport/App.java
@@ -0,0 +1,13 @@
+package selenium.Selenium_S2_ExtentReport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
